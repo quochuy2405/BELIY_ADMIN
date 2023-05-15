686c1ce8bbd7fbdabd2422b0eeddbdca5bcbaa0f
quochuy2405 <huy01682312920@gmail.com>
2023-05-16 02:06:00 +0700

    update

--- components/templates/Login.jsx
+++ components/templates/Login.jsx
@@ -74,45 +74,13 @@ const Login = ({ methods, handleSubmit }) => {
 									)}
 								/>
 							</div>
-							<div className="flex items-center justify-between">
-								<div className="flex items-start">
-									<div className="flex items-center h-5">
-										<input
-											id="remember"
-											aria-describedby="remember"
-											type="checkbox"
-											className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
-											required=""
-										/>
-									</div>
-									<div className="ml-3 text-sm">
-										<label htmlFor="remember" className="text-gray-500">
-											Remember me
-										</label>
-									</div>
-								</div>
-								<Link
-									href="#"
-									className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-								>
-									Forgot password?
-								</Link>
-							</div>
+
 							<button
 								type="submit"
 								className="w-full text-white border font-semibold bg-purple-700 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-base px-5 py-2.5 text-center"
 							>
 								Sign in
 							</button>
-							<p className="text-sm font-light text-gray-500 dark:text-gray-400">
-								Don’t have an account yet?{" "}
-								<Link
-									href="/register"
-									className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-								>
-									Sign up
-								</Link>
-							</p>
 						</form>
 					</div>
 				</div>
